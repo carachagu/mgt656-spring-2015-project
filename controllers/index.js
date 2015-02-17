7f629ce590727a2917aee3291ec356a1ef599438
alishar86 <alishar86@hotmail.com>
2015-02-17 18:39:46 +0000

    changes to the header

--- controllers/index.js
+++ controllers/index.js
@@ -8,8 +8,8 @@ var events = require('../models/events');
 function index (request, response) {
   var now = new Date();
   var contextData = {
-    'title': 'MGT 656',
-    'tagline': 'You are doomed (just kidding).',
+    'title': 'Carachugu',
+    'tagline': 'The number one event management tool for super Ninjas',
     'events': []
   };
   for(var i = 0; i<events.all.length; i++){
